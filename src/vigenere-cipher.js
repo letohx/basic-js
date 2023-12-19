65bdc938d287988484a79e0cab74dfeb0aec851d
Pavel Shlapak <letohx@gmail.com>
2023-12-20 01:25:13 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,63 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(massage, key) {
+    if (!massage || !key) throw new Error('Incorrect arguments!');
+
+    const result = [];
+    let indexKey = 0;
+
+    massage.toUpperCase().split('').forEach(char => {
+      if (char < 'A' || char > 'Z') {
+        result.push(char);
+      } else {
+        const massageCharIndex = char.charCodeAt(0) - 65;
+        const keyCharIndex = key[indexKey % key.length].toUpperCase().charCodeAt(0) - 65;
+        const encryptedCharIndex = `${((massageCharIndex + keyCharIndex + 26) % 26) + 65}`;
+        indexKey++;
+        result.push(String.fromCharCode(encryptedCharIndex));
+      }
+    });
+    return this.direct ? result.join('') : result.reverse().join('');
+  }
+  decrypt(encryptedMassage, key) {
+    if (!encryptedMassage || !key) throw new Error('Incorrect arguments!');
+
+    const result = [];
+    let indexKey = 0;
+
+    encryptedMassage.toUpperCase().split('').forEach(char => {
+      if (char < 'A' || char > 'Z') {
+        result.push(char);
+      } else {
+        const massageCharIndex = char.charCodeAt(0) - 65;
+        const keyCharIndex = key[indexKey % key.length].toUpperCase().charCodeAt(0) - 65;
+        const CharIndex = `${((massageCharIndex - keyCharIndex + 26) % 26) + 65}`;
+        indexKey++;
+        result.push(String.fromCharCode(CharIndex));
+      }
+    });
+    return this.direct ? result.join('') : result.reverse().join('');
   }
 }
 
