8f535f80fc418ea5c35e1a5a0f52befa8e7f63eb
Pavel Shlapak <letohx@gmail.com>
2023-12-23 18:45:58 +0300

    feat: add solution dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const object  = {};
+
+  for (let domain of domains) {
+    const domainLevelsArr = domain.split('.').reverse();
+    domainLevelsArr.forEach((level, index) => {
+      const dns = `.${domainLevelsArr.slice(0, index + 1).join('.')}`
+      object[dns] = object[dns] ?? 0;
+      object[dns] += 1;
+    });
+  }
+  return object;
 }
 
 module.exports = {
